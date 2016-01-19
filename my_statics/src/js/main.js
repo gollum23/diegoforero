ac426836c457cb60fdb3ce995f77fe60aca1646d
Diego Forero <gollum23@gmail.com>
2016-01-18 22:19:37 -0500

    Style home include cover video

--- my_statics/src/js/main.js
+++ my_statics/src/js/main.js
@@ -1,4 +1,58 @@
 (() => {
     "use strict";
+    scaleVideoContainer();
+
+    initBannerVideoSize('.Hero-wrapper .Hero-filter');
+    initBannerVideoSize('.Hero-wrapper .Hero-video');
+
+    $(window).on('resize', () => {
+        scaleVideoContainer();
+        scaleBannerVideoSize('.Hero-wrapper .Hero-filter');
+        scaleBannerVideoSize('.Hero-wrapper .Hero-video');
+    });
+
+
+    function scaleVideoContainer() {
+
+        let height = $(window).height();
+        let unitHeight = parseInt(height) + 'px';
+        $('.Hero').css('height',unitHeight);
+
+    }
+
+    function initBannerVideoSize(element){
+
+        $(element).each(function(){
+            $(this).data('height', $(this).height());
+            $(this).data('width', $(this).width());
+        });
+
+        scaleBannerVideoSize(element);
+    }
+
+    function scaleBannerVideoSize(element){
+
+        let windowWidth = $(window).width();
+        let windowHeight = $(window).height() + 5;
+        let videoWidth;
+        let videoHeight;
+
+        $(element).each(function(){
+            let videoAspectRatio = $(this).data('height')/$(this).data('width');
+
+            $(this).width(windowWidth);
+
+            if(windowWidth < 1000){
+                videoHeight = windowHeight;
+                videoWidth = videoHeight / videoAspectRatio;
+                $(this).css({'margin-top' : 0, 'margin-left' : -(videoWidth - windowWidth) / 2 + 'px'});
+
+                $(this).width(videoWidth).height(videoHeight);
+            }
+
+            $('.Hero .Hero-wrapper .Hero-video').addClass('fadeIn animated');
+
+        });
+    }
 
 })();
