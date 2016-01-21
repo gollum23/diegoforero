524d5750d93e49e7994f2e568a748ad05fd01b02
Diego Forero <gollum23@gmail.com>
2016-01-21 08:05:22 -0500

    Include queryset portfolio items in context

--- home/views.py
+++ home/views.py
@@ -1,5 +1,13 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+
+from .models import PortfolioItem
 
 
 class HomeView(TemplateView):
     template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        portfolio_items = PortfolioItem.objects.filter(published=True).order_by('-order')
+        context['portfolio'] = portfolio_items
+        return context
