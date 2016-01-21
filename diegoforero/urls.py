20ac49ff24cff5b6d2fccb3f710481d2f6ed6048
Diego Forero <gollum23@gmail.com>
2016-01-21 08:04:35 -0500

    Include url pattern for serve media files in dev environ

--- diegoforero/urls.py
+++ diegoforero/urls.py
@@ -13,11 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url, include
+from django.conf.urls.static import static
 from django.contrib import admin
+
 from home import urls as home_urls
 
 urlpatterns = [
     url(r'^', include(home_urls, namespace='home')),
     url(r'^admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
