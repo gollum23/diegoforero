dff54d611514f2631759474099054f61549de952
Diego Forero <gollum23@gmail.com>
2016-01-21 10:08:19 -0500

    Fix vars

--- bin/start_gunicorn.sh
+++ bin/start_gunicorn.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 NAME="diegoforero"                                  # Name of the application
-DJANGODIR=$HOME/diegoforero.xyz             # Django project directory
-SOCKFILE=$HOME/diegoforero.xyz/run/gunicorn.sock  # we will communicte using this unix socket
+DIR=""
+DJANGODIR=$DIR/diegoforero.xyz             # Django project directory
+SOCKFILE=$DIR/diegoforero.xyz/run/gunicorn.sock  # we will communicte using this unix socket
 USER=                                        # the user to run as
 GROUP=                                     # the group to run as
 NUM_WORKERS=3                                     # how many worker processes should Gunicorn spawn
@@ -13,7 +14,7 @@ echo "Starting $NAME as `whoami`"
 
 # Activate the virtual environment
 cd $DJANGODIR
-export WORKON_HOME=$HOME/.virtualenvs
+export WORKON_HOME=$DIR/.virtualenvs
 source /usr/local/bin/virtualenvwrapper.sh
 workon diegoforero
 export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
@@ -29,7 +30,7 @@ test -d $RUNDIR || mkdir -p $RUNDIR
 
 # Start your Django Unicorn
 # Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
-exec $WORKON_HOME/diegoforero/bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
+exec $DIR/.virtualenvs/diegoforero/bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
   --name $NAME \
   --workers $NUM_WORKERS \
   --user=$USER --group=$GROUP \
