4c352a3300dee9251ba733457bd66bc4df24eca5
Diego Forero <gollum23@gmail.com>
2021-03-18 19:53:18 -0500

    (feat): update ports

--- docker/prod/run-production.sh
+++ docker/prod/run-production.sh
@@ -3,4 +3,4 @@
 python manage.py migrate
 python manage.py collectstatic --noinput
 
-gunicorn -w 2 diegoforero.wsgi:application -b 0.0.0.0:8001  --log-level error --keep-alive 200 --env DJANGO_SETTINGS_MODULE=diegoforero.settings.production
+gunicorn -w 2 diegoforero.wsgi:application -b 0.0.0.0:8000  --log-level error --keep-alive 200 --env DJANGO_SETTINGS_MODULE=diegoforero.settings.production
